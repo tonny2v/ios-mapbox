3939f8d011c7168f307612f4e07b1241aab5954c
tonny <tonny.achilles@gmail.com>
2018-12-31 21:32:28 +0800

    Initial Commit

---
+++ cocoa-test/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  cocoa-test
+//
+//  Created by Jiangshan on 12/31/18.
+//  Copyright © 2018 Jiangshan. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ cocoa-test/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  cocoa-test
+//
+//  Created by Jiangshan on 12/31/18.
+//  Copyright © 2018 Jiangshan. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
